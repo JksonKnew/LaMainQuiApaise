674007a315acbed4be6170b9f766b235c00f9b86
BastienD <biloute.bd@gmail.com>
2024-04-09 22:15:15 +0200

    nav func

--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import './nav.scss'
+import { Link, useLocation } from "react-router-dom";
+import './nav.scss';
 
 function NavBar() {
+    const location = useLocation();
+
+    // Fonction pour déterminer si le chemin correspond à l'URL actuelle
+    const isActiveLink = (path) => {
+        // Comparer le chemin avec l'URL actuelle
+        return location.pathname === path;
+    };
+
     return (
         <React.Fragment>
             <div className="navbarContainer">
                 <div className="logoContainer">./</div>
                 <div className="navLinkContainer">
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/oizad">Error</Link>
+                    <Link to="/" className={isActiveLink("/") ? "navActive" : ""}>Home</Link>
+                    <Link to="/about" className={isActiveLink("/about") ? "navActive" : ""}>About</Link>
+                    <Link to="/oizad" className={isActiveLink("/oizad") ? "navActive" : ""}>Error</Link>
                 </div>
-
             </div>
         </React.Fragment>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
